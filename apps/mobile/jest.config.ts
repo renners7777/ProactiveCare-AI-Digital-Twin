7fc68ba4925ceff2c52fac7c79e7bdd757ed77e7
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-06-08 17:58:55 +0100

    Various packages changed

--- apps/mobile/jest.config.ts
+++ apps/mobile/jest.config.ts
@@ -2,21 +2,25 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'jest-expo',
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-  ],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', './src/test/setup.ts'],
+  setupFilesAfterEnv: ['./src/test/setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  moduleNameMapper: {
-    '\\.svg': '<rootDir>/src/test/__mocks__/svgMock.ts'
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/src/test/__mocks__/fileMock.js',
+  },
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
     '!src/test/**',
-    '!src/**/*.stories.{ts,tsx}'
-  ]
+    '!src/**/__mocks__/**',
+  ],
 };
 
 export default config;
