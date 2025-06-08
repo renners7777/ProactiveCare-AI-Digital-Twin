14572ed00635e9a308496ba54d0222d70846a27a
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-06-08 19:39:51 +0100

    Refactor Jest configuration and package.json scripts for improved clarity and consistency

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,22 +1,16 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFiles: ['<rootDir>/src/test/jest.setup.js'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+  setupFiles: [
+    '<rootDir>/src/test/jest.setup.js'
   ],
-  moduleNameMapper: {
-    '\\.svg': '<rootDir>/src/test/__mocks__/svgMock.js'
-  },
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect'
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        jsx: 'react'
-      }
-    }
-  },
-  testEnvironment: 'node',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   }
