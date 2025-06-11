32386243c7c6e1a334a38cec9610f2ba4ba02df1
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-06-11 17:47:18 +0100

    Various files added to mobile app

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,12 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      'babel-preset-expo',
-      '@babel/preset-typescript'
-    ],
+      'babel-preset-expo', '@babel/preset-typescript'],
     plugins: [
-      'react-native-reanimated/plugin'
-    ]
+      'react-native-reanimated/plugin',
+    ],
+      [
+        'module-resolver',
+        {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          alias: {
+            common: '../../common/src', 
+          },
+        },
+      ],
   };
 };
---
+++ apps/mobile/eas-build-pre-install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Running pre-install script for EAS in monorepo"
+yarn install --immutable
