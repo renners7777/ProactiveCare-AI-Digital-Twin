062f98fa3227f7698be835a5ebcb883b1ca6638a
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-06-15 21:26:26 +0100

    Updated app.json and babel.config.js files

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,44 +1,35 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          extensions: [
-            '.ios.ts',
-            '.android.ts',
-            '.ts',
-            '.ios.tsx',
-            '.android.tsx',
-            '.tsx',
-            '.jsx',
-            '.js',
-            '.json',
-          ],
-          alias: {
-            '@': './src',
-            'common': '../../packages/common/src',
-            '@screens': './src/screens',
-            '@components': './src/components',
-            '@services': './src/services',
-            '@utils': './src/utils',
-            '@hooks': './src/hooks',
-            '@contexts': './src/contexts',
-            '@constants': './src/constants',
-            '@assets': './assets'
-          }
+      ['module-resolver', {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          'common': '../../packages/common/src',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@contexts': './src/contexts',
+          '@constants': './src/constants',
+          '@assets': './assets'
         }
-      ],
-      ['nativewind/babel', { mode: 'compileTime' }]
-    ],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel']
-      }
-    }
+      }]
+    ]
   };
 };
