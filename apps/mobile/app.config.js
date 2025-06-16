e9e0839c99e73899a37f9d991d339b25e5e85edd
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-06-16 23:36:30 +0100

    Various changes made to debug Expo issues

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,8 +1,23 @@
 module.exports = {
-  name: 'ProactiveCare',
-  slug: 'proactivecare',
-  version: '1.0.0',
-  orientation: 'portrait',
-  userInterfaceStyle: 'light',
-  assetBundlePatterns: ['**/*']
+  name: "ProactiveCare",
+  slug: "proactivecare",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    }
+  }
 };
